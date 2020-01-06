8318ec2f797efdc1903adb5df3d6969fd33ca1e4
Spartak Fomenko <spartakfomenko28@gmail.com>
2020-01-06 15:22:50 +0600

    refactor code

--- src/hoc/withLoader.js
+++ src/hoc/withLoader.js
@@ -12,7 +12,10 @@ const withLoader = (WrapComponent, axios) => {
             };
 
 
-
+            axios.interceptors.request.use(request => {
+                this.setState({loader: true});
+                return request
+            });
             this.state.interceptorId = axios.interceptors.response.use(res => {
                 this.setState({loader: false});
                 return res;
